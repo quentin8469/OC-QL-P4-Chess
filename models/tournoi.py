2eb73df68a261d14bf0db5338c3d3e2b1ea83db5
quentin laparra <quentin.laparra@gmail.com>
2021-04-26 23:13:05 +0200

    Add param in constructor

--- models/tournoi.py
+++ models/tournoi.py
@@ -7,9 +7,16 @@ class Tournoi:
     All tournament informations
     """
     
-    def __init__(self):
+    def __init__(self, name, location, date, rondes, tournees, players, timer, description):
         """ initilisation of a tournament"""
-        pass
+        self.name = name
+        self.location = location
+        self.date = date
+        self.rondes = rondes
+        self.tournees_list = tournees
+        self.players_list = players
+        self.timer = timer
+        self.description = description
         
     def presentation():
         """ return the tournament infos """

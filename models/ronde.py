900c5819ded61d33632b25a56b18252703bcf7e6
quentin laparra <quentin.laparra@gmail.com>
2021-04-27 00:44:54 +0200

    Add function get first round

--- models/ronde.py
+++ models/ronde.py
@@ -10,3 +10,11 @@ class Ronde:
     def __init__(self):
         """ docting """
         pass
+		
+	def get_firstrounde(self):
+	    """ get the first round with Elo rank """
+		return
+		
+	def get_other_ronde(self):
+	    """ get the other round with the score """
+		return

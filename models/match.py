b6f69cd1d3376945e1865ea29e3458e6678f8aa0
quentin laparra <quentin.laparra@gmail.com>
2021-04-27 00:07:21 +0200

    Add param in constructor

--- models/match.py
+++ models/match.py
@@ -4,9 +4,10 @@
 
 class Match:
     """
-    doc string
+    Create Match
     """
     
-    def __init__(self):
-        """ docting """
-        pass
+    def __init__(self, player1, player2):
+        """ initialisation of a match """
+        self.player1 = player1
+		self.player2 = player2

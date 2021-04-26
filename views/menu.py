f2a8bf21d018f1d462098b8ee67f5e4e41136aac
quentin laparra <quentin.laparra@gmail.com>
2021-04-26 22:45:47 +0200

    Add function menu choice

--- views/menu.py
+++ views/menu.py
@@ -10,3 +10,13 @@ class Menu:
     def __init__(self):
         """ docting """
         pass
+        
+    def menu_choice(self):
+        """ print the menu choices """
+        print("welcome, please select your action and press Enter")
+        print("1.")
+        print("2.")
+        print("3.")
+        print("Q. Quit the application")
+        return input("Your choice: ")
+        

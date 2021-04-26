931c8e674a24a2a40b99eb2d2da8778fee623fdf
quentin laparra <quentin.laparra@gmail.com>
2021-04-27 00:32:45 +0200

    Add function get color

--- models/match.py
+++ models/match.py
@@ -10,4 +10,8 @@ class Match:
     def __init__(self, player1, player2):
         """ initialisation of a match """
         self.player1 = player1
-		self.player2 = player2
+        self.player2 = player2
+    
+    def get_color(self):
+        """ give the color white or black to a player"""
+        return

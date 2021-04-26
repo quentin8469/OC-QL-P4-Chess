3a5399a6e156b9a6972a4bdd953631c92757a9d1
quentin laparra <quentin.laparra@gmail.com>
2021-04-26 16:04:12 +0200

    Add constructor in class joueur

--- models/joueur.py
+++ models/joueur.py
@@ -7,6 +7,11 @@ class Joueur:
     doc string
     """
     
-    def __init__(self):
+    def __init__(self,lastname, firstname, birth_date, gender, elo):
         """ docting """
+		self.lastname = lastname
+		self.firstname = firstname
+		self.birth_date = birth_date
+		self.gender = gender
+		self.elo = elo
         pass

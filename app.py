0a8446f73cb201d790bf01716442aaddae57ac55
quentin laparra <quentin.laparra@gmail.com>
2021-04-26 22:45:03 +0200

    Add action in function main

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#from controller.controller import Controller
+from controller.controller import Controller
 
 def main():
     """ Start the application """

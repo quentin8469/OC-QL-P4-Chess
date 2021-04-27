dd11516e175480b5a9cba56e4d7e83bfd8073d6f
quentin laparra <quentin.laparra@gmail.com>
2021-04-27 22:30:15 +0200

    Correction name controllers

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from controller.controller import Controller
+from controllers.controller import Controller
+#from views.menu import Menu
 
 def main():
     """ Start the application """
-    Controller()
+    #test = Menu()
+    #test.menu_start()
+    #test.menu_report()
+    test = Controller()
+    test.start()
 
 
 if __name__ == "__main__":

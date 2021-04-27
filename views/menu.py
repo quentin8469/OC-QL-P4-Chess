63da1b7b1b9f839ac6cc1fe5bd73878ac2707084
quentin laparra <quentin.laparra@gmail.com>
2021-04-27 13:56:52 +0200

    Add function menu player

--- views/menu.py
+++ views/menu.py
@@ -12,7 +12,7 @@ class Menu:
         pass
         
     def menu_start(self):
-        """ print the menu choices """
+        """ print the start menu """
         print("welcome, please select your action and press Enter")
         print("1.New Tournament")
         print("2.Add player")
@@ -21,3 +21,13 @@ class Menu:
         print("5.Modif player")
         print("Q.Quit the application")
         return input("Your choice: ")
+    
+    def menu_player(self):
+        """ print the Player menu """
+        print("welcome, please select your action and press Enter")
+        print("1.New player")
+        print("2.Modif player")
+        print("3.Report")
+        print("Q.Return to start menu")
+        return input("Your choice: ")
+    

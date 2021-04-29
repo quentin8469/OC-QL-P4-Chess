5cb6ade33b680cd83e1e1e39f1d33d9959b8319d
quentin laparra <quentin.laparra@gmail.com>
2021-04-29 15:51:43 +0200

    Add function start, player, tournament, report

--- controllers/controller.py
+++ controllers/controller.py
@@ -10,25 +10,29 @@ class Controller:
     doc string
     """
     
-    def __init__(self):
+    def __init__(self, menu):
         """ docting """
-        pass
+        self.menu = menu
+        self.start_menu()
     
-    def start(self):
+    def start_menu(self):
         """ start the view menu_start"""
-        start = Menu()
-        start.menu_start()
-        test = input("Your choice: ")
-        if test == "1":
-           start.menu_tournament()
-        if test == "2":
-           start.menu_player()
-        if test == "3":
-           start.menu_report()
-        if test == "M":
-           start.menu_start()
-        
-    '''    
+        self.menu.menu_start()       
+        input_choice = input("Your choice: ")
+    
+    def player_menu(self):
+       """ start the player menu"""
+       self.menu.menu_player()
+    
+    def tournament_menu(self):
+       """ start the player menu"""
+       self.menu.menu_tournament()
+
+    def report_menu(self):
+       """ start the player menu"""
+       self.menu.menu_report()
+    
+	'''    
     def choice_menu(self):
        """ doc string"""
        choice = start.menu_start()

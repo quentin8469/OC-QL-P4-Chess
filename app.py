7d1d7b1ec23efbed095f34bf5290039cb349d0ad
quentin laparra <quentin.laparra@gmail.com>
2021-04-29 15:12:32 +0200

    delet def main

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from controllers.controller import Controller
-#from views.menu import Menu
+from views.menu import Menu
 
-def main():
-    """ Start the application """
-    #test = Menu()
-    #test.menu_start()
-    #test.menu_report()
-    test = Controller()
-    test.start()
 
 
 if __name__ == "__main__":
-    main()
+    menu = Menu()
+    view = Controller(menu)
+    view.start()

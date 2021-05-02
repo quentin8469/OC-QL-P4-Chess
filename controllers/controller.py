5fea57a57649ab4dd4f50b240909f71e9a194e01
quentin laparra <quentin.laparra@gmail.com>
2021-05-02 20:02:27 +0200

    WIP on choice menu start, remplace if by dict

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,101 +3,118 @@
 from models.joueur import Player
 from models.tournoi import Tournament
 
+
 class Controller:
     """
     This class control the menu views
     """
-    
+
     def __init__(self, menu):
         """ constructor initialisation """
         self.menu = menu
         self.start_menu()
-    
+
     def start_menu(self):
         """ start the view menu_start"""
-        self.menu.menu_start()       
+        self.menu.menu_start()
         input_choice = input()
         self.choice_menu_start(input_choice)
-    
+
     def player_menu(self):
-       """ start the player menu"""
-       self.menu.menu_player()
-       input_choice = input("Your choice: ")
-       self.choice_menu_player(input_choice)
+        """ start the player menu"""
+        self.menu.menu_player()
+        input_choice = input()
+        self.choice_menu_player(input_choice)
 
     def tournament_menu(self):
-       """ start the tournament menu"""
-       self.menu.menu_tournament()
-       input_choice = input("Your choice: ")
-       self.choice_menu_tournament(input_choice)
+        """ start the tournament menu"""
+        self.menu.menu_tournament()
+        input_choice = input()
+        self.choice_menu_tournament(input_choice)
 
     def report_menu(self):
-       """ start the report menu"""
-       self.menu.menu_report()
-       input_choice = input("Your choice: ")
-       self.choice_menu_report(input_choice)
+        """ start the report menu"""
+        self.menu.menu_report()
+        input_choice = input()
+        self.choice_menu_report(input_choice)
 
     def choice_menu_start(self, input_choice):
-       """ navigate in the menu_start"""
-       if input_choice == "1":
-           self.tournament_menu()          
-       if input_choice == "2":
-           self.player_menu()
-       if input_choice == "3":
-           self.report_menu()
-       if input_choice == "Q":
-           pass
-    
+        """ navigate in the menu_start"""
+        '''
+        #input_choice = input_choice
+        input_dict = {
+            "1": self.tournament_menu(),
+            "2": self.player_menu(),
+            "3": self.report_menu(),
+            "Q": "Au revoir",
+        }
+        while choice not in input_dict.keys():
+            choice = 
+            input_dict[choice]
+        '''
+        
+        if input_choice == "1":
+            self.tournament_menu()          
+        if input_choice == "2":
+            self.player_menu()
+        if input_choice == "3":
+            self.report_menu()
+        if input_choice == "Q":
+            pass
+        #input_dict = {"1" : self.tournament()}
+        #test = input_dict[input_choice]
+        
+
     def choice_menu_player(self, input_choice):
-       """ navigate in the menu_player"""
-       if input_choice == "1":
-           # New player
-           self.new_player()          
-       if input_choice == "2":
-           # Modif player
-           #self.modif_player()
-           pass
-       if input_choice == "3":
-           # Report
-           #self.report()
-           pass
-       if input_choice == "M":
-           # Return start menu
-           self.start_menu()
-    
+        """ navigate in the menu_player"""
+        if input_choice == "1":
+            # New player
+            self.new_player()
+        if input_choice == "2":
+            # Modif player
+            # self.modif_player()
+            pass
+        if input_choice == "3":
+            # Report
+            # self.report()
+            pass
+        if input_choice == "M":
+            # Return start menu
+            self.start_menu()
+
     def choice_menu_tournament(self, input_choice):
-       """ navigate in the menu_tournament"""
-       if input_choice == "1":
-           # New tournament
-           self.new_tournament()         
-       if input_choice == "2":
-           pass
-       if input_choice == "3":
-           pass
-       if input_choice == "4":
-           pass
-       if input_choice == "M":
-           self.start_menu()
-           
+        """ navigate in the menu_tournament"""
+        if input_choice == "1":
+            # New tournament
+            self.new_tournament()
+        if input_choice == "2":
+            pass
+        if input_choice == "3":
+            pass
+        if input_choice == "4":
+            pass
+        if input_choice == "M":
+            self.start_menu()
+
     def choice_menu_report(self, input_choice):
-       """ navigate in the menu_report"""
-       if input_choice == "1":
-           pass         
-       if input_choice == "2":
-           pass
-       if input_choice == "3":
-           pass
-       if input_choice == "4":
-           pass
-       if input_choice == "5":
-           pass
-       if input_choice == "6":
-           pass
-       if input_choice == "7":
-           pass
-       if input_choice == "M":
-           self.start_menu()
-    
+        """ navigate in the menu_report"""
+        if input_choice == "1":
+            pass
+        if input_choice == "2":
+            pass
+        if input_choice == "3":
+            pass
+        if input_choice == "4":
+            pass
+        if input_choice == "5":
+            pass
+        if input_choice == "6":
+            pass
+        if input_choice == "7":
+            pass
+        if input_choice == "M":
+            self.start_menu()
+
     def new_player(self):
         """ Create a new player"""
         self.menu.new_player_lname()
@@ -110,16 +127,16 @@ class Controller:
         gender = input()
         self.menu.new_player_elo()
         elo = input()
-        new_player = Player(lastname, firstname, birth_date, gender,elo)
-    
+        new_player = Player(lastname, firstname, birth_date, gender, elo)
+
     def modif_player(self):
         """ modif player data"""
         pass
-    
+
     def report():
         """ Create en report"""
         pass
-    
+
     def new_tournament(self):
         """ Create a new player"""
         self.menu.new_tournament_name()
@@ -134,6 +151,4 @@ class Controller:
         tournees = input()
         self.menu.new_tournament_description()
         description = input()
-        new_tournament = Tournament(name, location, date, rondes, tournees, 
-                                                                    description)
-    
+        new_tournament = Tournament(name, location, date, rondes, tournees, description)

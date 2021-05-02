dc8e3dd66353d62ad87cd9e6512630eb9d8d003c
quentin laparra <quentin.laparra@gmail.com>
2021-05-02 20:03:54 +0200

    PEP8

--- models/tournoi.py
+++ models/tournoi.py
@@ -7,15 +7,13 @@ class Tournament:
     All tournament informations
     """
     
-    def __init__(self, name, location, date, rondes, tournees, players, timer, description):
+    def __init__(self, name, location, date, rondes, tournees, description):
         """ initilisation of a tournament"""
         self.name = name
         self.location = location
         self.date = date
         self.rondes = rondes
         self.tournees_list = tournees
-        self.players_list = players
-        self.timer = timer
         self.description = description
         
     def presentation():

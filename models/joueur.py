6df69cbf7d5b524b639ba78969bfa12df9c17736
quentin laparra <quentin.laparra@gmail.com>
2021-05-03 17:19:01 +0200

    delete all methode, juste keep the methode serializaed player

--- models/joueur.py
+++ models/joueur.py
@@ -15,25 +15,6 @@ class Player:
         self.gender = gender
         self.elo = elo
     
-    def presentation(self):
-        """ return the player infos """
-        return
-    
-    def get_lname(self):
-        """ add last name """
-        return self.lastname
-    
-    def get_fname(self):
-        """ add first name """
-        return self.firstname
-    
-    def get_bdate(self):
-        """ add birth date """
-        return self.birth_date
-    
-    def get_gender(self):
-        """ add gender """
-        return self.gender
     
     def serialized_player(self):
         """" serialization for save in TinyDB """

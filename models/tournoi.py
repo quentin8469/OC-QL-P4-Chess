25802de7b073f2859c0839fdc4246c44e575618f
quentin laparra <quentin.laparra@gmail.com>
2021-05-05 15:56:31 +0200

    Refactoring methodes

--- models/tournoi.py
+++ models/tournoi.py
@@ -16,38 +16,12 @@ class Tournament:
         self.tournees_list = tournees
         self.description = description
         
-    def presentation():
-        """ return the tournament infos """
-        return
-    
-    def get_name(self):
-        """ add tournament name """
-        return self.name
-        
-    def get_location(self):
-        """ add tournament location """
-        return self.location
-
-    def get_date(self):
-        """ add tournament date """
-        return self.date
-        
-    def get_ronde(self):
-        """ add tournament rondes """
-        return self.rondes
-        
-    def get_tournees(self):
-        """ add tournament tournees """
-        return self.tournees_list
-
-    def get_players(self):
-        """ add tournament players_list """
-        return self.players_list
-        
-    def get_timer(self):
-        """ add tournament timer """
-        return self.timer
-
-    def get_description(self):
-        """ add tournament description """
-        return self.description
+    def serialized_tournament(self):
+        """" serialization for save in TinyDB """
+        serialized_tournament = {
+            'Tournament name': self.name, 
+            'Tournament location': self.location,
+            'Tournament date': self.date,
+            'Tournament description' : self.description,
+        }
+        return serialized_tournament

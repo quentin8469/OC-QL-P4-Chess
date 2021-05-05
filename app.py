345628211c41e298c06566e0ef53f5698fbd05f4
quentin laparra <quentin.laparra@gmail.com>
2021-05-05 23:40:52 +0200

    Edit app for intanciation of ControllerTournament

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from controllers.controller import Controller
+from controllers.controllerplayer import PlayerController
+from controllers.controllertournament import TournamentController
 from views.menu import Menu
 
 
 
 if __name__ == "__main__":
     view = Menu()
-    start = Controller(view)
+    #start = Controller(view)
+    #player = PlayerController(view)
+    tournament= TournamentController(view)

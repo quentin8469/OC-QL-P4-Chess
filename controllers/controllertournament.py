f7b9c8f0b1b9abb77b78f4a4cc64972c29768c49
quentin laparra <quentin.laparra@gmail.com>
2021-05-06 21:31:29 +0200

    PEP8

--- controllers/controllertournament.py
+++ controllers/controllertournament.py
@@ -5,18 +5,18 @@ from models.match import Match
 from models.ronde import Ronde
 from tinydb import TinyDB, Query
 
+
 class TournamentController:
     """
     All tt controlles
     """
-    
+
     def __init__(self, menu):
         """ constructor controller tt"""
         self.menu = menu
-        self.tournamentdb = TinyDB('tournament1.json')
+        self.tournamentdb = TinyDB("tournament1.json")
         self.tournamentquery = Query()
-        self.tournament_table = self.tournamentdb.table('tournament1')
-        self.tournament_menu()
+        self.tournament_table = self.tournamentdb.table("tournament1")
 
     def tournament_menu(self):
         """ start the tournament menu"""
@@ -26,10 +26,16 @@ class TournamentController:
         while input_choice not in input_check_list:
             input_choice = input()
         menu = input_check_list.index(input_choice)
-        menu_list = [self.new_tt, self.edit_tt, self.tt_list, self.load_tt, 
-                                           self.report_tt, self.menu.menu_start]
+        menu_list = [
+            self.new_tt,
+            self.edit_tt,
+            self.tt_list,
+            self.load_tt,
+            self.report_tt,
+            Controlerv2.menu_start,
+        ]
         menu_list[menu]()
-    
+
     def new_tt(self):
         """ doc """
         self.menu.new_tournament_name()
@@ -48,24 +54,22 @@ class TournamentController:
                                                                     description)
         self.tournament_table.insert(new_tournament.serialized_tournament())
         self.tournament_menu()
-    
+
     def edit_tt(self):
         """ doc """
         pass
-    
+
     def tt_list(self):
         """ doc """
         tournaments = self.tournament_table.all()
         for tournament in tournaments:
             self.menu.tournament_list(tournament)
         self.tournament_menu()
-    
+
     def load_tt(self):
         """ doc """
         pass
-    
+
     def report_tt(self):
         """ doc """
         pass
-    
-    

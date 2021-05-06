d94e258e66dd82a4a5e490d2739912973c814d2e
quentin laparra <quentin.laparra@gmail.com>
2021-05-06 21:33:40 +0200

    add other intance

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from controllers.controller import Controller
-from controllers.controllerplayer import PlayerController
-from controllers.controllertournament import TournamentController
+#from controllers.controller import Controller
+from controllers.controllerv2 import Controllerv2
+from controllers.controllerv3 import Controllerv3
+#from controllers.controllerplayer import PlayerController
+#from controllers.controllertournament import TournamentController
 from views.menu import Menu
 
 
 
 if __name__ == "__main__":
     view = Menu()
-    #start = Controller(view)
+    start = Controllerv3(view)
     #player = PlayerController(view)
-    tournament= TournamentController(view)
+    #tournament= TournamentController(view)

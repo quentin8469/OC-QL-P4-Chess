a603456c92029fc15a6e2ee610eebb66ddc19d46
quentin laparra <quentin.laparra@gmail.com>
2021-05-12 22:38:17 +0200

    Add instance attribut

--- models/tournoi.py
+++ models/tournoi.py
@@ -7,13 +7,16 @@ class Tournament:
     All tournament informations
     """
     
-    def __init__(self, name, location, date, rondes, tournees, description):
+    def __init__(self, name, location, st_date, end_date, description):
         """ initilisation of a tournament"""
         self.name = name
         self.location = location
-        self.date = date
-        self.rondes = rondes
-        self.tournees_list = tournees
+        self.start_date = st_date
+        self.end_date = end_date
+        self.rondes = 4
+        self.tournees_list = ()
+        self.players = []
+        self.time_control = []
         self.description = description
         
     def serialized_tournament(self):
@@ -21,9 +24,12 @@ class Tournament:
         serialized_tournament = {
             'Tournament_name': self.name, 
             'Tournament_location': self.location,
-            'Tournament_date': self.date,
+            'Tournament_start_date': self.start_date,
+            'Tournament_end_date': self.end_date,
             'Tournament_rondes': self.rondes,
             'Tournament_tournees': self.tournees_list,
+            'Tournament_players' : self.players
+            'Tournament_Tcontrol' : self.time_control
             'Tournament_description' : self.description,
         }
         return serialized_tournament

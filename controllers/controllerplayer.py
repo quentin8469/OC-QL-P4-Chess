4e5b8e6b08e4962252de7d892ccae6c966875f80
quentin laparra <quentin.laparra@gmail.com>
2021-05-12 04:21:04 +0200

    PEP8

--- controllers/controllerplayer.py
+++ controllers/controllerplayer.py
@@ -16,12 +16,13 @@ class PlayerController:
         self.playerquery = Query()
         self.player_table = self.playerdb.table("players2")
         self.player_menu()
-
+        #self.dict_choice()
+    '''
     def player_menu(self):
         """ start the player menu"""
         self.menu.menu_player()
         input_choice = input()
-        input_check_list = ["1", "2", "3", "4", "5", "6"]
+        input_check_list = ["1", "2", "3", "4", "5"]
         while input_choice not in input_check_list:
             input_choice = input()
         menu = input_check_list.index(input_choice)
@@ -30,28 +31,27 @@ class PlayerController:
             self.edit_player,
             self.players_list,
             self.search_player,
-            self.update_player,
-            self.menu.menu_start,
+            self.start_menu,
         ]
         menu_list[menu]()
-
     '''
+        
     def player_menu(self):
         """ start the player menu"""
+        #print("test")
         self.menu.menu_player()
-        for input in         
-        input_dict = {
+        input_choice = input()        
+        bob = {
             "1": self.new_player(),
-            "2": self.modif_player(),
-            "3": self.report(),
-            "4": "Au revoir",
+            "2": self.edit_player(),
+            "3": self.players_list(),
+            "4": self.search_player(),
+            "5": self.update_player(),
+            "6": self.menu.menu_start(),
         }
-        input = 0
-        while input != 4):
-            input =int(input())
-            if (input >=0 and input <4):
-                input_dict[input]()
-    '''
+        #bob.get(input_choice, 0)
+        bob[input_choice]()
+    
 
     def new_player(self):
         """ Create a new player"""
@@ -70,8 +70,14 @@ class PlayerController:
         self.player_menu()
 
     def edit_player(self):
-        """ modif player data"""
-        self.menu.edit_player()
+        """  edit player Elo"""
+        self.menu.new_player_lname()
+        input_player = input()
+        self.menu.new_player_elo()
+        input_elo = input()
+        self.player_table.update({'Elo': input_elo},
+                                self.playerquery.Last_name == f'{input_player}')
+        self.player_menu()
 
     def players_list(self):
         """ Create a list of all players"""
@@ -83,25 +89,28 @@ class PlayerController:
 
     def player_alpha_order(self):
         """ List of the player in alphabetique order"""
-        sorted()
-        pass
+        
+        players = self.player_table.all()
+        alpha_order = sorted(players,key=lambda x:x['Last_name'])
+        for alpha in alpha_order:
+            self.menu.player_list(alpha)
+        self.report_menu()        
 
     def player_classement_order(self):
         """ List of the player in classement order"""
-        sorted()
-        pass
-
-    def update_player(self):
-        """ serch player"""
-        db.uptdate
+        players = self.player_table.all()
+        classe_order = sorted(players,key=lambda x:x['Elo'], reverse=True)
+        for classse in classe_order:
+            self.menu.player_list(classse)
+        self.report_menu()
 
     def search_player(self):
         """ serch player"""
-        
         self.menu.new_player_lname()
         input_player = input()
-        player = self.player_table.search(self.playerquery.Last_name == f'{input_player}')
+        player = self.player_table.search(self.playerquery.Last_name 
+                                                           == f'{input_player}')
         for play in player:
-            self.menu.player_list(play)
+            self.menu.player_search(play)
         self.player_menu()
         

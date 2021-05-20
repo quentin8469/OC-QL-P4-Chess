759aa63064c01729dc2574d0cc5ae3ba0f51430f
quentin laparra <quentin.laparra@gmail.com>
2021-05-20 16:44:48 +0200

    PEP8

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #from controllers.controller import Controller
-from controllers.controllerv2 import Controllerv2
+
 from controllers.controllerv3 import Controllerv3
-from controllers.controllerplayer import PlayerController
-#from controllers.controllertournament import TournamentController
-from controllers.controllerv4 import ControllerChess
+
 
 
 

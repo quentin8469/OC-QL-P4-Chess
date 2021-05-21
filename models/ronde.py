a87d0e918e8d1061fef8fcf99f9d849649ddf262
quentin laparra <quentin.laparra@gmail.com>
2021-05-21 02:13:41 +0200

    Add def serialization

--- models/ronde.py
+++ models/ronde.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+#
 
 class Ronde:
     """
     doc string
     """
-    
-    def __init__(self):
+    #ROUND_MAX = 4
+    def __init__(self, name, lmatch):
         """ docting """
-		self.name =
-		self.sttime =
-		self.endtime =
-		self.lmatch = []
+        self.name = name
+        self.sttime = ""
+        self.endtime = ""
+        self.lmatch = lmatch
         
         
-    def get_firstrounde(self):
-        """ get the first round with Elo rank """
-        return
-        
-    def get_other_ronde(self):
-        """ get the other round with the player score """
-        return
+    def serialized_rounds(self):
+        """" serialization for save in TinyDB """
+        serialized_rounds = {
+            'Ronde': self.name, 
+            'Start_time': self.sttime,
+            'End_time': self.endtime,
+            'Matchs': self.lmatch,
+        }
+        return serialized_rounds
+    '''    
+    @classmethod
+    def deserializeplayer(cls, rondes):
+        """ deserialized rondes from bdd"""
+        name = rondes['Ronde']
+        "" = rondes ['Start_time']
+        "" = rondes ['End_time']
+        lmatch = rondes ['Matchs']
+        rounds = Ronde(name, lmatch)
+        return rounds
+    '''

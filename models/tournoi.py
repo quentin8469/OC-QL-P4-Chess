b7f5c0a4ab9f6f0aaf86bd04230ec78d5cb55063
quentin laparra <quentin.laparra@gmail.com>
2021-05-21 13:49:16 +0200

    Maj PEP 8

--- models/tournoi.py
+++ models/tournoi.py
@@ -6,8 +6,19 @@ class Tournament:
     """
     All tournament informations
     """
-    
-    def __init__(self, name, location, st_date, e_date, rondes, tt_list, pl_list, ttc, description):
+
+    def __init__(
+        self,
+        name,
+        location,
+        st_date,
+        e_date,
+        rondes,
+        tt_list,
+        pl_list,
+        ttc,
+        description,
+    ):
         """ initilisation of a tournament"""
         self.name = name
         self.location = location
@@ -18,44 +29,46 @@ class Tournament:
         self.tt_players = pl_list
         self.time_control = ttc
         self.description = description
-        
+
     def serialized_tournament(self):
         """" serialization for save in TinyDB """
         serialized_tournament = {
-            'Tournament_name': self.name, 
-            'Tournament_location': self.location,
-            'Tournament_start_date': self.start_date,
-            'Tournament_end_date': self.end_date,
-            'Tournament_rondes': self.rondes,
-            'Tournament_tournees': self.tournees_list,
-            'Tournament_players' : self.tt_players,
-            'Tournament_Tcontrol' : self.time_control,
-            'Tournament_description' : self.description,
+            "Tournament_name": self.name,
+            "Tournament_location": self.location,
+            "Tournament_start_date": self.start_date,
+            "Tournament_end_date": self.end_date,
+            "Tournament_rondes": self.rondes,
+            "Tournament_tournees": self.tournees_list,
+            "Tournament_players": self.tt_players,
+            "Tournament_Tcontrol": self.time_control,
+            "Tournament_description": self.description,
         }
         return serialized_tournament
-        
+
     @classmethod
     def deserializett(cls, tt):
         """ deserialized tournament from bdd"""
-        name = tt['Tournament_name']
-        location = tt['Tournament_location']
-        st_date = tt['Tournament_start_date']
-        e_date = tt['Tournament_end_date']
-        rondes = tt['Tournament_rondes']
-        tt_list = tt['Tournament_tournees']
-        pl_list = tt['Tournament_players']
-        ttc = tt['Tournament_Tcontrol']
-        description = tt['Tournament_description']
-       
-        tournament = Tournament(name, location, st_date, e_date,rondes, tt_list, pl_list, ttc, description)
-        
+        name = tt["Tournament_name"]
+        location = tt["Tournament_location"]
+        st_date = tt["Tournament_start_date"]
+        e_date = tt["Tournament_end_date"]
+        rondes = tt["Tournament_rondes"]
+        tt_list = tt["Tournament_tournees"]
+        pl_list = tt["Tournament_players"]
+        ttc = tt["Tournament_Tcontrol"]
+        description = tt["Tournament_description"]
+
+        tournament = Tournament(
+            name, location, st_date, e_date, rondes, tt_list, pl_list, ttc, description
+        )
+
         return tournament
-    
+
     def add_player(self, player):
         """ add player in tt_players """
-        
+
         self.tt_players.append(player)
-    
+
     def add_tournees(self, rounds):
         """ add all the rounds in tournnes_liste """
         self.tournees_list.append(rounds)

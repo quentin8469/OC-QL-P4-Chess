09e35d912c3fdf04a629608bb4f8db04a0573219
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-05-25 21:45:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-#from controllers.controller import Controller
-
-from controllers.controllerv3 import Controllerv3
+#!/usr/bin/env/python3
 
 
-
-
+from controllers.controllerplayer import PlayerController
+from controllers.controllertournament import TournamentController
+from controllers.controllerchess import ControllerChess
 
 if __name__ == "__main__":
-	start = Controllerv3()
-    #start = ControllerChess()
-    #player = PlayerController(view)
-    #tournament= TournamentController(view)
+    players = PlayerController()
+    tournoi = TournamentController()
+    start = ControllerChess(players, tournoi)

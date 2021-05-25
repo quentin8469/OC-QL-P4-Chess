db753c4ddfee3ecbb69b6ebdd6b1e7ca235591e7
quentin laparra <quentin.laparra@gmail.com>
2021-05-25 22:04:29 +0200

    Maj pep8

---
+++ views/playerviews.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env/python3
+
+
+class PlayerViews:
+    """
+    In this class we find all the views
+    """
+
+    def __init__(self):
+        """ docting """
+        pass
+
+    def menu_player(self):
+        """ print the Player menu """
+        print("--------- Player Menu ---------")
+        print("welcome, please select your action and press Enter")
+        print("1.New player")
+        print("2.Edit player Elo")
+        print("3.Player list")
+        print("4.Return to start menu")
+        print("-------------------------------")
+
+    def new_player_lname(self):
+        """ get the player last name """
+        print("Enter player Last Name: ")
+
+    def new_player_fname(self):
+        """ get the player firt name """
+        print("Enter player First Name: ")
+
+    def new_player_bdate(self):
+        """ get the player Birth date """
+        print("Enter player Birth date: dd/mm/yyyy")
+
+    def new_player_gender(self):
+        """ get the player gender """
+        print("Enter player Gender: Male /Femelle / Autre")
+
+    def new_player_elo(self):
+        """ get the player Elo """
+        print("Enter player Elo number: ")
+
+    def error_player_lname(self):
+        """ ask the correct player last name """
+        print("Please enter the correct player Last name: ")
+
+    def player_list(self, player):
+        """ give the liste of the players"""
+
+        print("---------------------------------")
+        print("Last_name:", player["Last_name"])
+        print("First_name:", player["First_name"])
+        print("Birth_date:", player["Birth_date"])
+        print("Gender:", player["Gender"])
+        print("Elo:", player["Elo"])
+        print("Score:", player["Score"])
+        print("---------------------------------")
+
+    def player_search(self, player):
+        """ give the search of the players"""
+
+        print("-------Your research result------------")
+        print("doc_id:", player.doc_id)
+        print("Last_name:", player["Last_name"])
+        print("First_name:", player["First_name"])
+        print("Birth_date:", player["Birth_date"])
+        print("Gender:", player["Gender"])
+        print("Elo:", player["Elo"])
+        print("Score:", player["Score"])
+        print("---------------------------------------")
---
+++ views/tournamentviews.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env/python3
+
+
+class TournamentViews:
+    """
+    In this class we find all the views
+    """
+
+    def __init__(self):
+        """ docting """
+        pass
+
+    def menu_tournament(self):
+        """ print the Tournament menu """
+        print("----------- Tournament Menu -----------")
+        print("welcome, please select your action and press Enter")
+        print("1.Create Tournament")
+        print("2.List of Tournament")
+        print("3.Add player in Tournament")
+        print("4.Return to start menu")
+        print("---------------------------------------")
+
+    def new_tournament_name(self):
+        """ get the tournament name"""
+        print("Enter Tournament name: ")
+
+    def new_tournament_location(self):
+        """ get the tournament location"""
+        print("Enter Tournament location: ")
+
+    def new_tournament_date(self):
+        """ get the tournament date"""
+        print("Enter Tournament date: dd/mm/yyyy ")
+
+    def new_tournament_timer(self):
+        """ get the tournament timer"""
+        print("Tournament timer: ")
+
+    def new_tournament_description(self):
+        """ get the tournament location"""
+        print("Enter Tournament description: ")
+
+    def tournament_list(self, tournament):
+        """ give the liste of the tournament"""
+
+        print("-------- Liste des tournois --------")
+        print("doc_id:", tournament.doc_id)
+        print("Tournament_name:", tournament["Tournament_name"])
+        print("Tournament_location:", tournament["Tournament_location"])
+        print("Tournament_start_date:", tournament["Tournament_start_date"])
+        print("Tournament_end_date:", tournament["Tournament_end_date"])
+        print("Tournament_rondes:", tournament["Tournament_rondes"])
+        print("Tournament_tournees:", tournament["Tournament_tournees"])
+        print("Tournament_players:", tournament["Tournament_players"])
+        print("Tournament_Tcontrol:", tournament["Tournament_Tcontrol"])
+        print("Tournament_description:", tournament["Tournament_description"])
+        print("---------------------------------")
+
+    def tournament_load(self, tournament):
+        """ give the liste of the tournament"""
+
+        print("-------- Données du tournoi chargé ---------")
+        print("Tournament_name:", tournament.name)
+        print("Tournament_location:", tournament.location)
+        print("Tournament_start_date:", tournament.start_date)
+        print("Tournament_end_date:", tournament.end_date)
+        print("Tournament_rondes:", tournament.rondes)
+        print("Tournament_tournees:", tournament.tournees_list)
+        print("Tournament_players:", tournament.tt_players)
+        print("Tournament_Tcontrol:", tournament.time_control)
+        print("Tournament_description:", tournament.description)
+        print("--------------------------------------------")
+
+    def add_player_confirm(self):
+        """ get the tournament name"""
+        print("Confirme player ? y/n ")
+
+    def tt_adding_player(self):
+        """ get the tournament name"""
+        print("Your tournament is full of players")
